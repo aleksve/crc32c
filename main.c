3f3187bffdc533584ac20a07fd93d5617609abc6
Aleksander <aleksve@gmail.com>
2024-01-28 16:11:44 +0100

    Forgot a special case where data is less than four bytes. Checking before processing data, not after

--- main.c
+++ main.c
@@ -19,12 +19,14 @@ uint32_t asm_crc32c(const char *bytes, size_t len){
         "mov rbx, %[data_pointer]\n"
         "add rbx, r9\n"
         "not_done_4:\n"
+        "cmp %[data_pointer], rbx\n"
+        "je done_4\n"
 
         "crc32  %[result], dword ptr [%[data_pointer]]\n"
 
         "add %[data_pointer], 4\n"
-        "cmp %[data_pointer], rbx\n"
         "jne not_done_4\n"
+        "done_4:\n"
 
         "sub %[len], r9\n"
         "add rbx, %[len]\n"
@@ -46,7 +48,8 @@ uint32_t asm_crc32c(const char *bytes, size_t len){
 
 void test() {
     // Test cases generated with https://crccalc.com/, Result in the CRC-32C algorithm
-    assert(asm_crc32c("AsdFJklo",0) ==0x5899CC2E) ;
+    //assert(asm_crc32c("AsdFJklo",0) ==0x5899CC2E) ;
+    assert(asm_crc32c("A",0) ==0xE16DCDEE) ;
     assert(asm_crc32c("AsdFJklof",0)==0x99878FEE) ;
     assert(asm_crc32c("Sixbyt",0)== 	0xF1119B6C) ;
     assert(asm_crc32c("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",0)==0x95DC2E4B) ;
