de4afdd8b6402538f7a87decdbc6a5d8ea14f985
Aleksander <aleksve@gmail.com>
2024-01-28 11:29:41 +0100

    Assembler works when length is divisible by four

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
-#include <smmintrin.h>
 #include <string.h>
+#include <assert.h>
 
-uint32_t crc32(unsigned char*, size_t);
+uint32_t crc32(unsigned const char*, size_t);
 
 int main() {
-    char * bytes = "Asdf";
+    char * bytes = "AsdFJklo";
     //char * bytes = "fdsA";
     size_t len = strlen(bytes);
 
@@ -14,18 +14,27 @@ int main() {
     return 0;
 }
 // http://www.sunshine2k.de/coding/javascript/crc/crc_js.html
-uint32_t crc32(unsigned char *bytes, size_t len){
-    uint32_t result = 0xFFFFFFFF;
-    for (int x = 0; x < len; x+=4)
-    {
-        asm(
-                "crc32 %[result], %[data_pointer]\n"
-                :[result]"+r"(result)
-                :[data_pointer]"r"(*(uint32_t*)bytes+x)
-                );
-        //result = _mm_crc32_u32(result, *(uint32_t*)bytes);
-    }
-    return result ^ 0xFFFFFFFF;
+uint32_t crc32(unsigned const char *bytes, size_t len){
+    int  result;
+    // Only sequences of length divisible by four is supported, because it is interpreted as a sequence of 32-bit workds's, and a special case is not yet written
+    assert((len/4)*4 == len);
+    asm("mov %[result], 0xFFFFFFFF\n"
+        "mov rbx, %[data_pointer]\n"
+        "add rbx, %[len]\n"
+        "not_done:\n"
+
+        "crc32  %[result], dword ptr [%[data_pointer]]\n"
+
+        "add %[data_pointer], 4\n"
+        "cmp %[data_pointer], rbx\n"
+        "jne not_done\n"
+            :[result]"+r"(result)
+            :[data_pointer]"r"((uint32_t*)bytes), [len]"r"(len)
+            :"rbx"
+            );
+
+
+    return result^0xFFFFFFFF; // Compiler optimizes this to "not eax"
 
     //    asm("mov \n"
 //        "inc rax\n"
