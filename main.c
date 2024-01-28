1d61edf77281f61f5c5d6fc8b074ed3008596f44
Aleksander <aleksve@gmail.com>
2024-01-28 12:20:24 +0100

    Now works with any length data

--- main.c
+++ main.c
@@ -3,48 +3,50 @@
 #include <string.h>
 #include <assert.h>
 
-uint32_t crc32(unsigned const char*, size_t);
+uint32_t asm_crc32c(unsigned const char *bytes, size_t len);
 
 int main() {
-    char * bytes = "AsdFJklo";
+    char * bytes = "AsdFJklof";
     //char * bytes = "fdsA";
     size_t len = strlen(bytes);
 
-    printf("Bytes: %s, len: %ld, result: %x\n", bytes, len, crc32(bytes, len));
+    printf("Bytes: %s, len: %ld, result: %x\n", bytes, len, asm_crc32c(bytes, len));
     return 0;
 }
 // http://www.sunshine2k.de/coding/javascript/crc/crc_js.html
-uint32_t crc32(unsigned const char *bytes, size_t len){
+uint32_t asm_crc32c(unsigned const char *bytes, size_t len){
     int  result;
-    // Only sequences of length divisible by four is supported, because it is interpreted as a sequence of 32-bit workds's, and a special case is not yet written
-    assert((len/4)*4 == len);
+
     asm("mov %[result], 0xFFFFFFFF\n"
+
+        "mov r9, %[len]\n"
+        "shr r9, 2\n"
+        "shl r9, 2\n"
+
         "mov rbx, %[data_pointer]\n"
-        "add rbx, %[len]\n"
-        "not_done:\n"
+        "add rbx, r9\n"
+        "not_done_4:\n"
 
         "crc32  %[result], dword ptr [%[data_pointer]]\n"
 
         "add %[data_pointer], 4\n"
         "cmp %[data_pointer], rbx\n"
-        "jne not_done\n"
+        "jne not_done_4\n"
+
+        "sub %[len], r9\n"
+        "add rbx, %[len]\n"
+        "cmp %[data_pointer], rbx\n"
+        "je end_of_data\n"
+        "crc32 %[result], byte ptr [%[data_pointer]]\n"
+        "inc %[data_pointer]\n"
+        "end_of_data:"
             :[result]"+r"(result)
             :[data_pointer]"r"((uint32_t*)bytes), [len]"r"(len)
-            :"rbx"
+            :"rbx", "r9"
             );
 
 
     return result^0xFFFFFFFF; // Compiler optimizes this to "not eax"
-
-    //    asm("mov \n"
-//        "inc rax\n"
-//        "mov %[result], rax"
-//
-//        :[result]"=r"(result)
-//        :[in]"r"(bytes), [len]"r"(len)
-//    : "rax"
-//                );
-
 }
 
 
